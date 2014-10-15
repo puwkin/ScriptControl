2b7984dc28f915c49a3de380ca8b5b118f69ee4e
Eddy Hintze <eddy.hintze@gmail.com>
2014-10-15 19:37:17 -0400

    Updated example module to show the correct structure of the modules.

--- src/scripts/example.py
+++ src/scripts/example.py
@@ -1,16 +1,21 @@
 
 class Example:
-
+    """
+    This is the default structure for each module
+    """
     def __init__(self):
         self._output = []
+        #trigger:
+        #   call     - only runs when called via api
+        #   interval - runs every x seconds
         self.trigger = 'call'
 
     def run(self):
         """
         Main function that is called
         """
-        self.output.append("I ran!")
-        return self.output
+        self._output.append("I ran!")
+        return self._output
 
     def get_output(self):
         return self._output
